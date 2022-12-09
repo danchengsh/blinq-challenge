c7420ca8be837cad85d6ab8a74068e825b9aeb89
Sing Hao Cheng <chengsh98@gmail.com>
2022-12-09 12:07:03 +1100

    Setup Homepage component

--- blinq-app/src/App.js
+++ blinq-app/src/App.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HeaderBar from './components/header-bar/header-bar.component';
+import Homepage from './components/homepage/homepage.component';
 import FooterBar from './components/footer-bar/footer-bar.component';
 
 const App = () => {
   return (
     <Fragment>
       <HeaderBar />
-      {/* <Routes>
-        <Route path='/' element={<HomePage />}>
+      <Routes>
+        <Route path='/' element={<Homepage />}>
         </Route>
-      </Routes> */}
+      </Routes>
       <FooterBar />
     </Fragment>
   );
---
+++ blinq-app/src/components/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import './homepage.styles.css';
+
+const Homepage = () => {
+    return (
+      <div className="homepage-container">
+        <div className="homepage-content-container">
+          <div className="title">A better way to enjoy every day.</div>
+          <div className="subtitle">Be the first to know when we launch.</div>
+          <button type="button">Request an invite</button>
+        </div>
+      </div>
+    );
+};
+
+export default Homepage;
