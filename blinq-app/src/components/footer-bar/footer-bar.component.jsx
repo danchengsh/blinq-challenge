ef1e9d1f2045a2e6c8520f582806fa7bbe4f4b00
Sing Hao Cheng <chengsh98@gmail.com>
2022-12-09 16:21:54 +1100

    Vertically center content in HeaderBar and FooterBar

--- blinq-app/src/components/footer-bar/footer-bar.component.jsx
+++ blinq-app/src/components/footer-bar/footer-bar.component.jsx
@@ -3,8 +3,10 @@ import './footer-bar.styles.css';
 const FooterBar = () => {
     return (
       <div className="footer-container">
-        <p className="footer-text">Made with love in Melbourne.</p>
-        <p className="footer-text">&#169; 2016 Broccoli & Co. All rights reserved.</p>
+        <div className="footer-content">
+          <p className="footer-text">Made with love in Melbourne.</p>
+          <p className="footer-text">&#169; 2016 Broccoli & Co. All rights reserved.</p>
+        </div>
       </div>
     );
 };
